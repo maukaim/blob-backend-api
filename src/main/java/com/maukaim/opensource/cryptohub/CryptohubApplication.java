dcc0db4be1dab62d7e2f1bf7b3d264963e9e1a6c
Julien Elkaim <julienelk@gmail.com>
2021-07-11 16:00:43 +0200

    First Commit

---
+++ src/main/java/com/maukaim/opensource/cryptohub/CryptohubApplication.java
@@ -0,0 +1,13 @@
+package com.maukaim.opensource.cryptohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptohubApplication.class, args);
+	}
+
+}
